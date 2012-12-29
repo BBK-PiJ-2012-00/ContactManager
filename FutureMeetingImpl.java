1e7ab17e3f800763cded45f28a6b111c01042a5f
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-12-29 18:47:45 +0000

    Initial draft, contains constructor

---
+++ FutureMeetingImpl.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+
+	public FutureMeetingImpl(Set<Contact> contacts, Calendar date) {
+		super(contacts, date);
+	}
+
+
+
+
+
+
+
+}

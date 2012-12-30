03c24a94cf6b78f0de0a407a1cec13f01c63e310
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-12-30 12:54:13 +0000

    addFutureMeeting() method in progress - initial workthrough

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -15,16 +15,23 @@ import java.util.Set;
 */
 public class ContactManagerImpl implements ContactManager { 
 
-/**
-* Add a new meeting to be held in the future. 
-* 
-* @param contacts a list of contacts that will participate in the meeting 
-* @param date the date on which the meeting will take place 
-* @return the ID for the meeting 
-* @throws IllegalArgumentException if the meeting is set for a time in the past,
-* or if any contact is unknown / non-existent.
-*/
-int addFutureMeeting(Set<Contact> contacts, Calendar date);
+	/**
+	* Add a new meeting to be held in the future. 
+	* 
+	* @param contacts a list of contacts that will participate in the meeting 
+	* @param date the date on which the meeting will take place 
+	* @return the ID for the meeting 
+	* @throws IllegalArgumentException if the meeting is set for a time in the past,
+	* or if any contact is unknown / non-existent.
+	*/
+	int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+		//Return meeting ID
+		//Check that contacts exist
+		//Checks that the date is not in the past
+		Meeting futureMeeting = new FutureMeetingImpl(contacts, date);
+		return futureMeeting.getID();
+	}
+	
 
 /**
 * Returns the PAST meeting with the requested ID, or null if it there is none. 

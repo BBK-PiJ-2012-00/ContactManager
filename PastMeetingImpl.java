13e2cfb642fed5e344d396458d5dd0e1a342fde0
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-12-30 12:44:51 +0000

    Constructor added, may need additional parameters depending on usage with ContactManager class; notes field added.

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -4,15 +4,22 @@
 * It includes your notes about what happened and what was agreed. 
 */
 public class PastMeetingImpl extends Meeting implements PastMeeting { 
+	private String notes = "";
+	
+	public PastMeetingImpl(String notes) {
+		this.notes = this.notes + notes;
+	}
 	
 
-/**
-* Returns the notes from the meeting. 
-* 
-* If there are no notes, the empty string is returned. 
-* 
-* @return the notes from the meeting. 
-*/
-String getNotes();
+	/**
+	* Returns the notes from the meeting. 
+	* 
+	* If there are no notes, the empty string is returned. 
+	* 
+	* @return the notes from the meeting. 
+	*/
+	public String getNotes() {
+		return notes;
+	}
 
 }

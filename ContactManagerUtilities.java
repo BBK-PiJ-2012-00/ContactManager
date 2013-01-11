8354b3906caad227def68222c7f6d7d21241c0d6
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-01-11 17:17:49 +0000

    Corrected some general errors: ensured that all methods are static, as they belong to the class. Ensured that the necessary import statements are present at the top of the class. Major changes to the selectAttendees() method: it was rather uneconomical to check whether the user-input contact ID numbers are valid here, when there is a method in the ContactManager class for this purpose (getContacts(int... ids)). Therefore, I've decided to have the method display the contactList to the user from which they can make their ID selections; these selections are checked in the Utility class, and the selectAttendees() method now returns an array or Integers, to be used as a parameter in the getContacts() method in the ContactManager class. This saves repeating code, and the bulk of the 'utility' work is still done in the Utility class.

--- ContactManagerUtilities.java
+++ ContactManagerUtilities.java
@@ -1,11 +1,17 @@
 /**
-* A class that handles the user interface of ContactManager, and takes user entries.
+* A class that handles some of the user interface of ContactManager, and takes/validates user entries.
 */
 
-import java.io.IllegalArgumentException;
+import java.util.regex.*;
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContactManagerUtilities {
-	private IllegalArgumentException illegalArgEx = new IllegalArgumentException();
+	private static IllegalArgumentException illegalArgEx = new IllegalArgumentException();
 
 	public static void displayWelcome() {
 		System.out.println("*************************************************************");
@@ -27,48 +33,42 @@ public class ContactManagerUtilities {
 	}
 	
 	public static int validateOption(int min, int max) {//to validate user choices from numerical menus
+		int selection;
 		try {
-			int selection = Integer.parseInt(System.console().readLine());
+			selection = Integer.parseInt(System.console().readLine());
 			if (selection < min || selection > max) {
 		 		throw illegalArgEx;
 		 	}
-		 	return selection;
-		 }		
+		 	
+		 }	
+		 catch (NumberFormatException ex) {
+		 	System.out.println("Please enter a number that corresponds to an option on the menu.");
+		 	return validateOption(min, max);
+		 }	
 		 catch (IllegalArgumentException ex) {
 		 	System.out.println("Invalid option. Please make a selection from the menu.");
 		 	return validateOption(min, max);		 	
 		 }
-		 catch (NumberFormatException ex) {
-		 	System.out.println("Please enter a number that corresponds to an option on the menu."):
-		 }
-	}
+		 return selection;
+	}	
 	
-	public static void subMenuOne(Set<Contact> contacts) {//takes contactList from ContactManager class for display
-	//set of contacts and date
-		Set<Contact> attendeeList = new HashSet<Contact>();
-		System.out.println("*** Add a future meeting");
-		System.out.println("Your contacts: ");
-		Iterator<Contact> iterator = contacts.iterator();
-		while (iterator.hasNext()) {
-			Contact c = iterator.next();
-			System.out.println("c.getName() " + "c.getId()");
-		}
-		System.out.println("Please enter the IDs of the contacts who are attending, separated" +
-		" by a comma. Finish by pressing RETURN.");
-		String entry = System.console().readLine();
-		
-	}
-	//Instead, perhaps have method to facilitate and check selection of users, and entry of date,
-	//so that the checking is done here
 	
-	public set<Contact> selectAttendees(Set<Contact> contactList) {
-		Set<Contact> attendeeList = new HashSet<Contact>();
+	/**
+	* A method that allows the user to select contacts by ID no.
+	* to be added to a set, which is usually a required parameter 
+	* for creating a new meeting. Prints contactList Set for 
+	* ease of reference.
+	*/
+	public static Integer[] selectAttendees(Set<Contact> contactList) {
+		//Set<Contact> attendeeList = new HashSet<Contact>();
+		List<Integer> attendeeList = new ArrayList<Integer>();
+		Integer[] attendees;
 		System.out.println("Your contact list: ");
 		if (contactList.isEmpty()) {
 			System.out.println("<Empty> You will need to add contacts to your contact list before creating meetings.");
-			return null;//if null is returned, ContactManager will need to take corresponding action
+			return null;//This sends the user back to the main menu via a break statement in ContactManager
 		}
-		for (Contact c : contactList) {
+		for (Contact c : contactList) {//Prints contactList for ease of reference
 			System.out.println("c.getName() " + "c.getId()");
 		}
 		System.out.println("Please enter the IDs of the contacts who are attending, separated" +
@@ -79,32 +79,37 @@ public class ContactManagerUtilities {
 			if (userEntry.equals("back")) {
 				return null;//allows user to cancel and return to main menu - ensure main menu handles null appropriately
 			}
-			boolean verified = validateCommaString(userEntry);//determine whether input is valid
+			boolean verified = validateCommaString(userEntry);//determines whether input is valid
 			if (!verified) {
 				throw illegalArgEx;
 			}
-			Scanner sc = new Scanner(userEntry);
+			Scanner sc = new Scanner(userEntry);//if input is valid, creates an arrayList with the user input numbers
 			Pattern delimiterPattern = Pattern.compile("[\\s]?[,][\\s]?");
 			sc.useDelimiter(delimiterPattern);
 			do {
 				int selection = sc.nextInt();
-				for (Contact c : contactList) {
-					if (c.getId() == selection) {//match the int with contact ID in contactList
-						attendeeList.add(c);
-					}
-				}
-			} while (sc.hasNextInt());					
+				attendeeList.add(selection);
+			} while (sc.hasNextInt());
+			attendees = attendeeList.toArray(new Integer[0]);//converts to array for use in ContactManager getContacts() method
+			//so that it can be ascertained whether the user has selected contacts who exist
+				
+						
 		}
 		catch (IllegalArgumentException ex) {
 			System.out.println("Error! Please enter the contact ID numbers separated by a comma " +
 			"i.e. 1, 2, 3 or 1,2,3 and finish by pressing RETURN.");
-			attendeeList = selectAttendees(contactList);
+			attendees = selectAttendees(contactList);
 		}
-		return attendeeList;	
+		return attendees;	
 	}
 	
-	
-	public boolean validateCommaString(String userEntry) {
+	/*
+	* A method to validate a user-entered String of
+	* digits separated by commas and spaces.
+	* Is reasonably lenient to ensure user friendliness:
+	* 1,2,3 , 4, 5, 6 is considered valid.
+	*/
+	public static boolean validateCommaString(String userEntry) {
 		Pattern pattern = Pattern.compile("([0-9][0-9]*[\\s]?[,][\\s]?)*[0-9][0-9]*");
 		Matcher m = pattern.matcher(userEntry);//match given input against pattern
 		boolean verified = m.matches();//upshot - matches or doesn't
@@ -114,9 +119,17 @@ public class ContactManagerUtilities {
 		return false;
 	}
 		
-		boolean b = Pattern.matches("a*b", "aaaaab");
-		// if userEntry matches this ok, then proceed to make delimiter String and parse
-		//in selectAttendees method
+		
 		
 }
+
+/**
+int selection = sc.nextInt(); //repeating getContacts(int... ids).... THINK! Do this in other class?
+				for (Contact c : contactList) {
+					if (c.getId() == selection) {//match the int with contact ID in contactList
+						attendeeList.add(c);
+					}
+				}
+			} while (sc.hasNextInt());			
+*/
 	

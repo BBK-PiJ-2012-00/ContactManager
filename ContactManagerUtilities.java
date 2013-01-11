d2c6e9cbeb6f97772b1f19087cd9bcba9893b061
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-01-11 18:36:03 +0000

    Building createDate() method, which asks the user to enter a date and verifies (with the help of validateDate()) whether or not it is valid.

--- ContactManagerUtilities.java
+++ ContactManagerUtilities.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class ContactManagerUtilities {
 	private static IllegalArgumentException illegalArgEx = new IllegalArgumentException();
@@ -54,13 +56,13 @@ public class ContactManagerUtilities {
 	
 	
 	/**
-	* A method that allows the user to select contacts by ID no.
-	* to be added to a set, which is usually a required parameter 
-	* for creating a new meeting. Prints contactList Set for 
-	* ease of reference.
+	* A method that allows the user to select contacts by ID number. These numbers
+	* are then added to an array, which is returned to the ContactManager class
+	* for validation with the getContacts(int... ids) method. 
+	* Prints contactList Set for ease of reference.
 	*/
 	public static Integer[] selectAttendees(Set<Contact> contactList) {
-		//Set<Contact> attendeeList = new HashSet<Contact>();
+		
 		List<Integer> attendeeList = new ArrayList<Integer>();
 		Integer[] attendees;
 		System.out.println("Your contact list: ");
@@ -91,9 +93,7 @@ public class ContactManagerUtilities {
 				attendeeList.add(selection);
 			} while (sc.hasNextInt());
 			attendees = attendeeList.toArray(new Integer[0]);//converts to array for use in ContactManager getContacts() method
-			//so that it can be ascertained whether the user has selected contacts who exist
-				
-						
+			//so that it can be ascertained whether the user has selected contacts who exist						
 		}
 		catch (IllegalArgumentException ex) {
 			System.out.println("Error! Please enter the contact ID numbers separated by a comma " +
@@ -118,18 +118,76 @@ public class ContactManagerUtilities {
 		}
 		return false;
 	}
+	
+	
+	public static Calendar createDate() {
+		Calendar date;
+		int day;
+		int month;
+		int year;
+		int[] dateArray = new int[3];
+		try {
+			System.out.println("Please enter the date of the meeting in dd.mm.yyyy format: ");
+			String userEntry = System.console().readLine();
+			if (userEntry.equals("back")) {
+				return null;//allows user to cancel and return to main menu 
+			}
+			boolean verified = validateDateEntry(userEntry);//determines whether input is valid
+			if (!verified) {
+				throw illegalArgEx;
+			}
+			Scanner sc = new Scanner(userEntry);//if input is valid, creates a date
+			Pattern delimiterPattern = Pattern.compile("[\\.]");
+			sc.useDelimiter(delimiterPattern);
+			for (int i = 0; i < 3; i++) {
+				dateArray[i] = sc.nextInt();
+			}
+			day = dateArray[0];
+			month = dateArray[1] - 1;//Calendar interprets January as 0, February as 1 etc
+			year = dateArray[2];
+			if (month == 1 && day == 29) {
+				if ((year % 4 0= 0) && (year % 100 != 0)) {
+					//this is ok - it's a leap year
+				}
+				if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+					//this is ok - it's a leap year
+				}
+				else {
+					throw illegalDateException;//Feb only has 29 days in leap year
+				}
+			}
+			
+			
+			
+			//A year will be a leap year if it is divisible by 4 but not by 100. 
+		//If a year is divisible by 4 and by 100, it is not a leap year unless it is also divisible by 400
+								
+		}
+		catch (IllegalArgumentException ex) {
+			System.out.println("Error! Please enter the date in dd.mm.yyyy format " +
+			"i.e. 01.11.2013 and finish by pressing RETURN.");
+			date = createDate();
+		}
+		return date;	
+	}
+	
+	
+	public boolean validateDateEntry(String userEntry) {
+		//checks date format, allowing d.m.yyyy or dd.mm.yyyy. Rules out invalid days and months i.e. 59.40.2001
+		Pattern pattern = Pattern.compile("(([0]?[1-9])|([1-2][0-9])|([3][0-1]))[\\.](([0]?[1-9])|([1][0-2]))[\\.][2][0][0-9][0-9]");
+		Matcher m = pattern.matcher(userEntry);//match given input against pattern
+		boolean verified = m.matches();//upshot - matches or doesn't
+		if (verified) {
+			return true;
+		}
+		return false;
+	}
+		//MUST DEAL WITH MONTHS THAT HAVE 30/31 DAYS!!!
+		.
+	
+		
 		
 		
 		
 }
 
-/**
-int selection = sc.nextInt(); //repeating getContacts(int... ids).... THINK! Do this in other class?
-				for (Contact c : contactList) {
-					if (c.getId() == selection) {//match the int with contact ID in contactList
-						attendeeList.add(c);
-					}
-				}
-			} while (sc.hasNextInt());			
-*/
-	

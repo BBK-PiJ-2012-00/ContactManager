865e5be2bb5456fe8933eff7f770aaa673102c28
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-01-14 15:32:12 +0000

    Added addNotes() to the interface, as this is a key functionality of the class.

--- PastMeeting.java
+++ PastMeeting.java
@@ -5,13 +5,20 @@
 */
 public interface PastMeeting extends Meeting { 
 
-/**
-* Returns the notes from the meeting. 
-* 
-* If there are no notes, the empty string is returned. 
-* 
-* @return the notes from the meeting. 
-*/
-String getNotes();
+	/**
+	* Returns the notes from the meeting. 
+	* 
+	* If there are no notes, the empty string is returned. 
+	* 
+	* @return the notes from the meeting. 
+	*/
+	String getNotes();
+
+	/**
+	* Adds notes to a past meeting.
+	*
+	* @param the notes to be added.
+	*/
+	void addNotes(String notes);
 
 }

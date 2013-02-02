edf7ad06b7743d9618a90eb33edaaa3494aa16a9
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-02-02 14:33:39 +0000

    Any notes added at a later time are not added onto a new line for clarity.

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,16 +1,12 @@
 import java.io.Serializable;
 
-/** 
-* A contact is a person we are making business with or may do in the future. 
-* 
-* Contacts have an ID (unique), a name (probably unique, but maybe 
-* not), and notes that the user may want to save about them. 
-*/
+
 public class ContactImpl implements Contact, Serializable { 
 	private int id;
 	private String name;
 	private String notes = "";
 	private static int idAssigner = 0; //to facilitate assignment of an id
+
 	
 	public ContactImpl(String name) {
 		this.name = name;
@@ -45,7 +41,7 @@ public class ContactImpl implements Contact, Serializable {
 	}
 
 	public void addNotes(String note) {
-		notes = notes + note + " "; //so that notes String is not overwritten if addNotes is called more than once
+		notes = notes + note + "\n"; //so that notes String is not overwritten if addNotes is called more than once
 	}
 
 }

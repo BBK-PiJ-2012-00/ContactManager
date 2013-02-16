c30b0a2ef8a0418218d40ee511979a13da6e1dd9
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2013-02-16 14:13:21 +0000

    Removed commented code and duplicate comments.

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -1,17 +1,16 @@
 import java.util.Calendar;
 import java.util.Set;
-/** 
-* A meeting that was held in the past. 
-* 
-* It includes your notes about what happened and what was agreed. 
-*/
+
+
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting { 
 	private String notes = "";
+	
 	
 	public PastMeetingImpl(Set<Contact> contacts, Calendar date, String notes) {
 		super(contacts, date);
 		this.notes = this.notes + " " + notes;
 	}
+	
 	
 	/**
 	* For use when a FutureMeeting is converted to a
@@ -22,18 +21,12 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 		this.notes = this.notes + " " + notes;
 	}
 	
+	
 	public void addNotes(String notes) {
 		this.notes = this.notes + " " + notes;
-	}
-	
+	}	
 
-	/**
-	* Returns the notes from the meeting. 
-	* 
-	* If there are no notes, the empty string is returned. 
-	* 
-	* @return the notes from the meeting. 
-	*/
+	
 	public String getNotes() {
 		return notes;
 	}
